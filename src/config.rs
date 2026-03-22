9f8bbebb2e4c51e0f217f9ce496af994140870c9
ycstech <y_ch_s@qq.com>
2026-03-23 03:34:01 +0800

    改为自己的ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.ycsit.cn"];
+pub const RS_PUB_KEY: &str = "mhib5VVrotGt7wZtO8OVZ5462sXNpVYWYHBrsa2mQAw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
